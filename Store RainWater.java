7b18b335060183d79fd0f0cf8a8946757617ee2b
BhuvanChaithanya <99655691+BhuvanChaithanya@users.noreply.github.com>
2023-05-11 22:40:04 +0530

    Create Store RainWater.java
    the question is unsolved open to ideas i couldn't get the question anyway

---
+++ Store RainWater.java
@@ -0,0 +1,33 @@
+/*Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+Input Format
+
+First line of each test case or query contains an integer 'N' representing the size of the first array/list.
+Second line contains 'N' single space separated integers representing the elements in the array/list.
+Constraints
+
+n == height.length
+1 <= n <= 2 * 10^4
+0 <= height[i] <= 10^5
+Output Format
+
+Value showing how much water we can store
+Sample Input 0
+
+12
+0 1 0 2 1 0 1 3 2 1 2 1
+Sample Output 0
+
+6
+Explanation 0
+
+The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+Sample Input 1
+
+6
+4 2 0 3 2 5
+Sample Output 1
+
+9*/
+
